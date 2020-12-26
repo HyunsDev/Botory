7faec902c04509d5e7523c0b2ffe4d2137125ba3
Undecember <ehdtjskaqhd@gmail.com>
2020-12-27 03:20:52 +0900

    Add banish

--- cmds.py
+++ cmds.py
@@ -52,3 +52,36 @@ class Core(commands.Cog):
         else:
             who = discord.utils.get(ctx.guild.members, id = int(who[3:-1]))
             del dc[ctx.guild].reaction_macro[who]
+
+    @commands.command(name = 'banish')
+    async def banish(self, ctx, who):
+        global dc
+        await ctx.message.delete()
+        if ctx.guild not in dc: dc[ctx.guild] = GuildData(ctx.guild)
+        if ctx.author.top_role.name != '서버장': return
+        who = discord.utils.get(ctx.guild.members, id = int(who[3:-1]))
+        if who in dc[ctx.guild].banishdata: await ctx.channel.send('<@%d> 님은 이미 유배중입니다.'%who.id, allowed_mentions = discord.AllowedMentions.none(), delete_after=1.0)
+        else:
+            onick = who.nick
+            if who.nick == None: await who.edit(nick = '[유배중] ' + who.name)
+            else: await who.edit(nick = '[유배중] ' + who.nick)
+            orls = []
+            for rl in who.roles:
+                if rl.name in adroles: orls.append(rl)
+            await who.remove_roles(*orls)
+            dc[ctx.guild].banishdata[who] = (onick, orls)
+            await ctx.channel.send('<@%d> 님을 유배했습니다.'%who.id, allowed_mentions = discord.AllowedMentions.none(), delete_after=1.0)
+
+    @commands.command(name = 'forgive')
+    async def forgive(self, ctx, who):
+        global dc
+        await ctx.message.delete()
+        if ctx.author.top_role.name != '서버장': return
+        who = discord.utils.get(ctx.guild.members, id = int(who[3:-1]))
+        if who not in dc[ctx.guild].banishdata: await ctx.channel.send('<@%d> 님은 유배중이 아닙니다.'%who.id, allowed_mentions = discord.AllowedMentions.none(), delete_after=1.0)
+        else:
+            onick, orls = dc[ctx.guild].banishdata[who]
+            del dc[ctx.guild].banishdata[who]
+            await who.edit(nick = onick)
+            await who.add_roles(*orls)
+            await ctx.channel.send('<@%d> 님을 복귀시켰습니다.'%who.id, allowed_mentions = discord.AllowedMentions.none(), delete_after=1.0)
--- db.py
+++ db.py
@@ -23,3 +23,4 @@ class GuildData:
         self.cnls = self.ChannelsData()
         self.reaction_macro = dict()
         self.maxmsglen = 200
+        self.banishdata = dict()
